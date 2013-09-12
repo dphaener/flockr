baf8c0f8dcd00022ebe38dc0b4c0822f2936c0ad
Darin Haener <dphaener@gmail.com>
2013-09-11 20:54:59 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flockr::Application.config.secret_key_base = '1baa2abe480659d89c826dc0267c1335001ac43d497c3cf34693623ca156d3094d70ef08b9f89bbc85e317faf7a69c01a5d307fb1ea769ea304c8089c9216d34'
