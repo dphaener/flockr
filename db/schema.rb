62d0c1109cfa0cfe76855dbf2aec2cb53ee4990f
Darin Haener <dphaener@gmail.com>
2013-09-18 18:26:04 -0700

    Fixed all but one rspec test.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130913022641) do
+ActiveRecord::Schema.define(version: 20130916224750) do
 
   create_table "photos", force: true do |t|
     t.string   "file_name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20130913022641) do
     t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
 end
--- spec/views/photos/new.html.erb_spec.rb
+++ spec/views/photos/new.html.erb_spec.rb
@@ -4,19 +4,21 @@ describe "photos/new" do
   before(:each) do
     assign(:photo, stub_model(Photo,
       :file_name => "MyString",
-      :path => "MyString"
+      :path => "MyString",
+      :user_id => 1,
     ).as_new_record)
     
     assign(:user, stub_model(User,
-      :id => 1,
+      :user_id => 1,
       :username => "MyString",
       :email => "MyString@email.com"
-    ))
+    ).as_new_record)
   end
 
   it "renders new photo form" do
     render
-
+    user = User.create!(user_id: 1, username: "MyString", email: "MyString@email.com")
+    photo = Photo.create!(user_id: 1, path: "MyString", file_name: "MyString")
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form[action=?][method=?]", user_photos_path(user_id: 1), "post" do
       assert_select "input#photo_file_name[name=?]", "photo[file_name]"
