51659ffb4417816d2720d6b02453d3a3d332212c
Darin Haener <dphaener@gmail.com>
2013-09-19 18:21:24 -0700

    Minor change

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,11 @@ class User < ActiveRecord::Base
   has_many :photos, dependent: :destroy
   has_secure_password
   
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
   validates_presence_of :email
   validates_uniqueness_of :email
-  validates_format_of :email, :with => /.+(@).+(.).+/
+  validates_format_of :email, :with => VALID_EMAIL_REGEX
   validates_presence_of :username
   validates_uniqueness_of :username
   validates_length_of :password, :minimum => 6
